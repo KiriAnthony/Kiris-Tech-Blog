a3f88f1454ebf04ea7fa52b4bdd295702f1dffd2
Kiri Anthony <Kiriko.anthony@gmail.com>
2022-04-04 18:03:31 -0700

    updates

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -23,7 +23,7 @@ router.get('/', (req, res) => {
   })
     .then(dbPostData => {
       const posts = dbPostData.map(post => post.get({ plain: true }));
-      res.render('dashboard', { posts, loggedIn: true });
+      res.render('home', { posts, loggedIn: true });
     })
     .catch(err => {
       console.log(err);
